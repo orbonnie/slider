3add629b21bb04f87d0e21cef979eb8e9bb8545a
Typist_b <40219335+orbonnie@users.noreply.github.com>
2018-12-15 10:24:23 -0800

    Add files via upload

---
+++ fixer.js
@@ -0,0 +1,25 @@
+/* Pseudo code
+    1.
+
+
+    Cache DOM/pics
+    ________________________*/
+// let setUp= $('#setUp');
+// let board= $setUp.find('#board');
+// let piece = $board.find('.piece');
+
+
+
+
+// let tiles = [];
+// //     [1, 2, 3],
+// //     [4, 5, 6],
+// //     [7, 8, 9]
+// // ];
+//
+// for(let i=0; i< 10; i++){
+
+
+
+
+
